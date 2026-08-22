18cddcea92733c3fee2ab6da094421453cfad0f1
ShareMax <mdcw1103@gmail.com>
2026-08-22 19:29:59 +0800

    style(web): narrow home options sidebar

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -108,7 +108,7 @@ export function Home() {
                 </p>
               </div>
 
-              <aside className="flex w-full shrink-0 flex-col gap-5 lg:w-80 xl:w-96">
+              <aside className="flex w-full shrink-0 flex-col gap-5 lg:w-65 xl:w-75">
                 <div className="flex flex-col gap-2">
                   <Label htmlFor="title">{t('field.title')}</Label>
                   <Input
